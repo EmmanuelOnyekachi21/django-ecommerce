36d64c119933e1bfeb31adae3ebea58bf1df21eb
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-12-14 15:08:22 +0100

    Added doctring for the CustomUserAdmin class

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,16 @@ from .models import CustomUser
 # Register your models here.
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the CustomerUser model.
+    This class customizes the display and functionality of the CustomUser model
+    within the Django admin interface.
+
+    Attributes:
+        - add_fieldsets (tuple): Defines the fields and layout for adding new
+        users in the admin interface.  It specifies which fields are shown
+        during the creation of a new CustomUser.
+    """
     add_fieldsets = (
         # The outer tuple contains a single item (None, {...}), where None
         # represents a section title for the form.
