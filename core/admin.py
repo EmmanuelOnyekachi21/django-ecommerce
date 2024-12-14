145f2ad4c271713e025fdf4064c296efd7d9c6e5
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-12-14 14:38:37 +0100

    Add admin customization for CustomUser model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_fieldsets = (
+        # The outer tuple contains a single item (None, {...}), where None
+        # represents a section title for the form.
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name',
+                       'last_name', 'password1', 'password2', 'city', 'state',
+                       'address', 'phone', 'is_staff', 'is_active'
+                    )
+        }),
+    )
